8820c848c4684010189092665c0eefbf9c5e7935
Symbioquine <30754460+symbioquine@users.noreply.github.com>
2022-07-13 11:47:08 -0700

    Fix title query selector and wrap in anonymous function

--- src/script.js
+++ src/script.js
@@ -1,59 +1,61 @@
-let chatToggleBtn = document.querySelector(
-    ".toolbar-button-with-badge > .toolbox-button > div > .toolbox-icon"
-);
+(function() {
 
-// If the chat window is closed, open it to access the chats;
-if (!chatToggleBtn.classList.contains("toggled")) {
-    chatToggleBtn.click();
-}
+  let chatToggleBtn = document.querySelector(
+      ".toolbar-button-with-badge > .toolbox-button > div > .toolbox-icon"
+  );
 
-let chatContainer = document.querySelector("#chatconversation");
+  // If the chat window is closed, open it to access the chats;
+  if (!chatToggleBtn.classList.contains("toggled")) {
+      chatToggleBtn.click();
+  }
 
-// remote or local groups - to get direct children
-let descendents = chatContainer.getElementsByClassName('chat-message-group');
+  let chatContainer = document.querySelector("#chatconversation");
 
-// array of strings
-let jitsiChat = [];
+  // remote or local groups - to get direct children
+  let descendents = chatContainer.getElementsByClassName('chat-message-group');
 
-for (let i = 0; i < descendents.length; ++i) {
+  // array of strings
+  let jitsiChat = [];
 
-    let currentElement = descendents[i],
-        totalChildren = currentElement.childElementCount;
+  for (let i = 0; i < descendents.length; ++i) {
 
-    let user = document.querySelector("#chatconversation > div:nth-child(" + (i + 1) + ") > div:nth-child(1) > div > div > div > div.display-name").innerText;
+      let currentElement = descendents[i],
+          totalChildren = currentElement.childElementCount;
 
-    for (let j = 1; j <= totalChildren; ++j) {
-        
-        let query = "#chatconversation > div:nth-child(" + (i + 1) + ") > div:nth-child(" + j + ") > div > div > div > div.usermessage";
-        
-        let userMessage = document.querySelector(query).innerText;
-        
-        let stringEntry = user + ": " + userMessage;
-        jitsiChat.push(stringEntry);
-    }
-}
+      let user = document.querySelector("#chatconversation > div:nth-child(" + (i + 1) + ") > div:nth-child(1) > div > div > div > div.display-name").innerText;
 
-let meetName = document.querySelector(
-    "#videoconference_page > div.subject.visible > div > div.subject-info > span.subject-text"
-).innerText;
+      for (let j = 1; j <= totalChildren; ++j) {
+          
+          let query = "#chatconversation > div:nth-child(" + (i + 1) + ") > div:nth-child(" + j + ") > div > div > div > div.usermessage";
+          
+          let userMessage = document.querySelector(query).innerText;
+          
+          let stringEntry = user + ": " + userMessage;
+          jitsiChat.push(stringEntry);
+      }
+  }
 
-let finalFileName = meetName + ".txt";
+  let meetName = document.querySelector("#videoconference_page div.subject-text").innerText;
 
-let finalChat = jitsiChat.join("\r\n");
+  let finalFileName = meetName + ".txt";
 
-/* *
- * Downloads the final data in a .txt format.
- *
- * @param {string} filename - The string to be saved as file name.
- * @param {string} text - The data in a single string.
- * @returns {void}
- */
-((filename, text) => {
-    var element = document.createElement("a");
-	element.setAttribute(
-		"href",
-		"data:text/plain;charset=utf-8," + encodeURIComponent(text)
-	);
-	element.setAttribute("download", filename);
-	element.click();
-})(finalFileName, finalChat)
+  let finalChat = jitsiChat.join("\r\n");
+
+  /* *
+   * Downloads the final data in a .txt format.
+   *
+   * @param {string} filename - The string to be saved as file name.
+   * @param {string} text - The data in a single string.
+   * @returns {void}
+   */
+  ((filename, text) => {
+      var element = document.createElement("a");
+	  element.setAttribute(
+		  "href",
+		  "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+	  );
+	  element.setAttribute("download", filename);
+	  element.click();
+  })(finalFileName, finalChat)
+  
+})();
